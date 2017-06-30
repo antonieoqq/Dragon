7f924722f8ff18fa16fa759295d8cd6c300eb07d
antonieoqq <55801684.com>
2017-06-30 18:06:32 +0800

    DB状态机代码

--- DragonUnity/Assets/Scripts/Controllers/DBStateMachine.cs
+++ DragonUnity/Assets/Scripts/Controllers/DBStateMachine.cs
@@ -14,13 +14,70 @@ public enum ECompMode
     NotEqual,
 }
 
+public enum ECheckMode
+{
+    Compare,
+    IsTrue,
+}
+
 public abstract class DBCondition
 {
-    public abstract bool IsTrue();
+    public DBCondition(string name) { CheckParamName = name; }
+    public string CheckParamName { get; private set; }
+    public abstract bool Pass();
+}
+
+public class DBConditionScalar : DBCondition
+{
+    public DBConditionScalar(string name, ECompMode mode, float v) : base(name)
+    {
+        CompMode = mode;
+        CompareValue = v;
+    }
+    public ECompMode CompMode { get; private set; }
+    public float CompareValue { get; private set; }
+
+    public override bool Pass()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class DBConditionInteger : DBCondition
+{
+    public DBConditionInteger(string name, ECompMode mode, int v) : base(name)
+    {
+        CompMode = mode;
+        CompareValue = v;
+    }
+
+    public ECompMode CompMode { get; private set; }
+    public int CompareValue { get; private set; }
+
+    public override bool Pass()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class DBConditionBool : DBCondition
+{
+    public DBConditionBool(string name, bool v) : base(name)
+    {
+        RequiredValue = v;
+    }
+
+    public bool RequiredValue { get; private set; }
+
+    public override bool Pass()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class DBTransition
 {
+    private DBState _targetState;
     private LinkedList<DBCondition> _conditions;
 }
 
@@ -43,13 +100,13 @@ public class DBStateMachine
     }
 
     // 为了避免box/unbox，将3种基本类型的参数封装成类
-    private abstract class StateParamBase
+    public abstract class StateParamBase
     {
         public abstract bool IsComparable();
         public abstract bool IsBoolean();
     }
 
-    private abstract class StateParam : StateParamBase
+    public abstract class StateParam : StateParamBase
     {
         public StateParam(string name, EParamType type) { ParamName = name; ParamType = type; }
 
@@ -70,118 +127,65 @@ public class DBStateMachine
 
         public override sealed bool IsBoolean()
         {
-            switch (ParamType) {
-                case EParamType.Scalar:
-                case EParamType.Integer:
-                    return true;
-                case EParamType.Bool:
-                default:
-                    return false;
-            }
+            return !IsComparable();
         }
 
-        public virtual bool CompareWith(ECompMode compMode, float v)
-        {
-            if (!IsComparable())
-                Debug.LogWarning(ParamName + " is not comparable!");
+        //protected bool CompareFloat(ECompMode compMode, float v1, float v2)
+        //{
+        //    switch (compMode) {
+        //        case ECompMode.Less: return v1 < v2;
+        //        case ECompMode.LessOrEqual: return v1 <= v2;
+        //        case ECompMode.Equal: return v1 == v2;
+        //        case ECompMode.GreaterOrEqual: return v1 >= v2;
+        //        case ECompMode.Greater: return v1 > v2;
+        //        case ECompMode.NotEqual: return v1 != v2;
+        //        default:
+        //            Debug.LogWarning("Compare failed: " + ParamName);
+        //            return false;
+        //    }
+        //}
 
-            return false;
-        }
-
-        public virtual bool CompareWith(ECompMode compMode, int v)
-        {
-            if (!IsComparable())
-                Debug.LogWarning(ParamName + " is not comparable!");
-
-            return false;
-        }
-
-        public virtual bool IsTrue()
-        {
-            if (IsComparable())
-                Debug.LogWarning(ParamName + " is not a boolean parameter!");
-
-            return false;
-        }
-
-        protected bool CompareFloat(ECompMode compMode, float v1, float v2)
-        {
-            switch (compMode) {
-                case ECompMode.Less: return v1 < v2;
-                case ECompMode.LessOrEqual: return v1 <= v2;
-                case ECompMode.Equal: return v1 == v2;
-                case ECompMode.GreaterOrEqual: return v1 >= v2;
-                case ECompMode.Greater: return v1 > v2;
-                case ECompMode.NotEqual: return v1 != v2;
-                default:
-                    Debug.LogWarning("Compare failed: " + ParamName);
-                    return false;
-            }
-        }
-
-        protected bool CompareInt(ECompMode compMode, int v1, int v2)
-        {
-            switch (compMode) {
-                case ECompMode.Less: return v1 < v2;
-                case ECompMode.LessOrEqual: return v1 <= v2;
-                case ECompMode.Equal: return v1 == v2;
-                case ECompMode.GreaterOrEqual: return v1 >= v2;
-                case ECompMode.Greater: return v1 > v2;
-                case ECompMode.NotEqual: return v1 != v2;
-                default:
-                    Debug.LogWarning("Compare failed: " + ParamName);
-                    return false;
-            }
-        }
+        //protected bool CompareInt(ECompMode compMode, int v1, int v2)
+        //{
+        //    switch (compMode) {
+        //        case ECompMode.Less: return v1 < v2;
+        //        case ECompMode.LessOrEqual: return v1 <= v2;
+        //        case ECompMode.Equal: return v1 == v2;
+        //        case ECompMode.GreaterOrEqual: return v1 >= v2;
+        //        case ECompMode.Greater: return v1 > v2;
+        //        case ECompMode.NotEqual: return v1 != v2;
+        //        default:
+        //            Debug.LogWarning("Compare failed: " + ParamName);
+        //            return false;
+        //    }
+        //}
     }
 
-    private class ParamScalar : StateParam
+    public class ParamScalar : StateParam
     {
         public ParamScalar(string name, float v) : base(name, EParamType.Scalar)
         {
             ParamValue = v;
         }
         public float ParamValue;
-
-        public override bool CompareWith(ECompMode compMode, float v)
-        {
-            return CompareFloat(compMode, ParamValue, v);
-        }
-
-        public override bool CompareWith(ECompMode compMode, int v)
-        {
-            return CompareInt(compMode , (int)ParamValue, v);
-        }
     }
 
-    private class ParamInt : StateParam
+    public class ParamInt : StateParam
     {
         public ParamInt(string name, int v) : base(name, EParamType.Integer)
         {
             ParamValue = v;
         }
         public int ParamValue;
-
-        public override bool CompareWith(ECompMode compMode, float v)
-        {
-            return CompareFloat(compMode, ParamValue, v);
-        }
-
-        public override bool CompareWith(ECompMode compMode, int v)
-        {
-            return CompareInt(compMode, ParamValue, v);
-        }
     }
 
-    private class ParamBool : StateParam
+    public class ParamBool : StateParam
     {
         public ParamBool(string name, bool v) : base(name, EParamType.Bool)
         {
             ParamValue = v;
         }
         public bool ParamValue;
-
-        public override bool IsTrue() { return ParamValue; }
     }
 
     public bool DoseParamExist(string paramName)
@@ -189,60 +193,50 @@ public class DBStateMachine
         return _parameters.ContainsKey(paramName);
     }
 
-    public void AddScalarParam(string paramName, float paramValue)
+    public StateParam GetParam(string name)
     {
-        if (!DoseParamExist(paramName)) {
-            _parameters.Add(paramName, new ParamScalar(paramName, paramValue));
-            return;
+        if (DoseParamExist(name)) {
+            return _parameters[name];
         }
-        LogKeyConfliction(paramName);
+        return null;
     }
 
-    public void AddIntegerParam(string paramName, int paramValue)
+    public ParamScalar AddScalarParam(string paramName, float paramValue)
     {
         if (!DoseParamExist(paramName)) {
-            _parameters.Add(paramName, new ParamInt(paramName, paramValue));
-            return;
+            var newParam = new ParamScalar(paramName, paramValue);
+            _parameters.Add(paramName, newParam);
+            return newParam;
         }
         LogKeyConfliction(paramName);
+        return null;
     }
 
-    public void AddBoolParam(string paramName, bool paramValue)
+    public ParamInt AddIntegerParam(string paramName, int paramValue)
     {
         if (!DoseParamExist(paramName)) {
-            _parameters.Add(paramName, new ParamBool(paramName, paramValue));
-            return;
+            var newParam = new ParamInt(paramName, paramValue);
+            _parameters.Add(paramName, newParam);
+            return newParam;
         }
         LogKeyConfliction(paramName);
+        return null;
+    }
+
+    public ParamBool AddBoolParam(string paramName, bool paramValue)
+    {
+        if (!DoseParamExist(paramName)) {
+            var newParam = new ParamBool(paramName, paramValue);
+            _parameters.Add(paramName, newParam);
+            return newParam;
+        }
+        LogKeyConfliction(paramName);
+        return null;
     }
 
     public bool RemoveParam(string name)
     {
         return _parameters.Remove(name);
-    }
-
-    public bool CompareParamWith(string name, ECompMode compMode, float v)
-    {
-        if (DoseParamExist(name))
-            return _parameters[name].CompareWith(compMode, v);
-        LogKeyNotFound(name);
-        return false;
-    }
-
-    public bool CompareParamWith(string name, ECompMode compMode, int v)
-    {
-        if (DoseParamExist(name))
-            return _parameters[name].CompareWith(compMode, v);
-        LogKeyNotFound(name);
-        return false;
-    }
-
-    public bool IsParamTrue(string name)
-    {
-        if (DoseParamExist(name))
-            return _parameters[name].IsTrue();
-        LogKeyNotFound(name);
-        return false;
     }
 
     private void LogKeyConfliction(string paramName)
