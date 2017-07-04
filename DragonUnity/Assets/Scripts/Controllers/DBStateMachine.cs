800bf20c9e9c8a50606f1bcbb3ce58f3b47a4208
antonieoqq <55801684.com>
2017-07-04 18:05:25 +0800

    DB状态机代码更新

--- DragonUnity/Assets/Scripts/Controllers/DBStateMachine.cs
+++ DragonUnity/Assets/Scripts/Controllers/DBStateMachine.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum ECompMode
+public enum EDBCompMode
 {
     Less,
     LessOrEqual,
@@ -14,89 +14,115 @@ public enum ECompMode
     NotEqual,
 }
 
-public enum ECheckMode
+public enum EDBCheckMode
 {
     Compare,
     IsTrue,
 }
 
-public abstract class DBCondition
+public enum EDBParamType
 {
-    public DBCondition(string name) { CheckParamName = name; }
-    public string CheckParamName { get; private set; }
-    public abstract bool Pass();
-}
-
-public class DBConditionScalar : DBCondition
-{
-    public DBConditionScalar(string name, ECompMode mode, float v) : base(name)
-    {
-        CompMode = mode;
-        CompareValue = v;
-    }
-    public ECompMode CompMode { get; private set; }
-    public float CompareValue { get; private set; }
-
-    public override bool Pass()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class DBConditionInteger : DBCondition
-{
-    public DBConditionInteger(string name, ECompMode mode, int v) : base(name)
-    {
-        CompMode = mode;
-        CompareValue = v;
-    }
-
-    public ECompMode CompMode { get; private set; }
-    public int CompareValue { get; private set; }
-
-    public override bool Pass()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class DBConditionBool : DBCondition
-{
-    public DBConditionBool(string name, bool v) : base(name)
-    {
-        RequiredValue = v;
-    }
-
-    public bool RequiredValue { get; private set; }
-
-    public override bool Pass()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class DBTransition
-{
-    private DBState _targetState;
-    private LinkedList<DBCondition> _conditions;
-}
-
-public class DBState
-{
-    private LinkedList<DBTransition> _transitions;
+    Scalar,
+    Integer,
+    Bool,
 }
 
 public class DBStateMachine
 {
-    private Dictionary<string, StateParam> _parameters;
-    private Dictionary<string, DBState> _states;
-    private DBState _currentState;
 
-    public enum EParamType
+    public abstract class DBCondition
     {
-        Scalar,
-        Integer,
-        Bool,
+        public DBCondition(string name) { CheckParamName = name; }
+        public string CheckParamName { get; private set; }
+    }
+
+    public abstract class DBConditionComparable : DBCondition
+    {
+        public DBConditionComparable(string name) : base(name) { }
+
+        protected bool CompareFloat(EDBCompMode compMode, float v1, float v2)
+        {
+            switch (compMode) {
+                case EDBCompMode.Less: return v1 < v2;
+                case EDBCompMode.LessOrEqual: return v1 <= v2;
+                case EDBCompMode.Equal: return v1 == v2;
+                case EDBCompMode.GreaterOrEqual: return v1 >= v2;
+                case EDBCompMode.Greater: return v1 > v2;
+                case EDBCompMode.NotEqual: return v1 != v2;
+                default:
+                    Debug.LogWarning("Compare failed: " + CheckParamName);
+                    return false;
+            }
+        }
+
+        protected bool CompareInt(EDBCompMode compMode, int v1, int v2)
+        {
+            switch (compMode) {
+                case EDBCompMode.Less: return v1 < v2;
+                case EDBCompMode.LessOrEqual: return v1 <= v2;
+                case EDBCompMode.Equal: return v1 == v2;
+                case EDBCompMode.GreaterOrEqual: return v1 >= v2;
+                case EDBCompMode.Greater: return v1 > v2;
+                case EDBCompMode.NotEqual: return v1 != v2;
+                default:
+                    Debug.LogWarning("Compare failed: " + CheckParamName);
+                    return false;
+            }
+        }
+    }
+
+    public class DBConditionScalar : DBConditionComparable
+    {
+        public DBConditionScalar(string name, EDBCompMode mode, float v) : base(name)
+        {
+            CompMode = mode;
+            CompareValue = v;
+        }
+        public EDBCompMode CompMode { get; private set; }
+        public float CompareValue { get; private set; }
+    }
+
+    public class DBConditionInteger : DBCondition
+    {
+        public DBConditionInteger(string name, EDBCompMode mode, int v) : base(name)
+        {
+            CompMode = mode;
+            CompareValue = v;
+        }
+
+        public EDBCompMode CompMode { get; private set; }
+        public int CompareValue { get; private set; }
+    }
+
+    public class DBConditionBool : DBCondition
+    {
+        public DBConditionBool(string name, bool v) : base(name)
+        {
+            RequiredValue = v;
+        }
+
+        public bool RequiredValue { get; private set; }
+    }
+
+    public class DBTransition
+    {
+        public DBTransition()
+        {
+            Conditions = new LinkedList<DBCondition>();
+        }
+
+        public string TargetStateName { get; private set; }
+        public LinkedList<DBCondition> Conditions { get; private set; }
+    }
+
+    public class DBState
+    {
+        public DBState()
+        {
+            Transitions = new LinkedList<DBTransition>();
+        }
+
+        public LinkedList<DBTransition> Transitions { get; private set; }
     }
 
     // 为了避免box/unbox，将3种基本类型的参数封装成类
@@ -108,18 +134,18 @@ public class DBStateMachine
 
     public abstract class StateParam : StateParamBase
     {
-        public StateParam(string name, EParamType type) { ParamName = name; ParamType = type; }
+        public StateParam(string name, EDBParamType type) { ParamName = name; ParamType = type; }
 
         public string ParamName { get; private set; }
-        public EParamType ParamType { get; private set; }
+        public EDBParamType ParamType { get; private set; }
 
         public override sealed bool IsComparable()
         {
             switch (ParamType) {
-                case EParamType.Scalar:
-                case EParamType.Integer:
+                case EDBParamType.Scalar:
+                case EDBParamType.Integer:
                     return true;
-                case EParamType.Bool:
+                case EDBParamType.Bool:
                 default:
                     return false;
             }
@@ -129,41 +155,11 @@ public class DBStateMachine
         {
             return !IsComparable();
         }
-
-        //protected bool CompareFloat(ECompMode compMode, float v1, float v2)
-        //{
-        //    switch (compMode) {
-        //        case ECompMode.Less: return v1 < v2;
-        //        case ECompMode.LessOrEqual: return v1 <= v2;
-        //        case ECompMode.Equal: return v1 == v2;
-        //        case ECompMode.GreaterOrEqual: return v1 >= v2;
-        //        case ECompMode.Greater: return v1 > v2;
-        //        case ECompMode.NotEqual: return v1 != v2;
-        //        default:
-        //            Debug.LogWarning("Compare failed: " + ParamName);
-        //            return false;
-        //    }
-        //}
-
-        //protected bool CompareInt(ECompMode compMode, int v1, int v2)
-        //{
-        //    switch (compMode) {
-        //        case ECompMode.Less: return v1 < v2;
-        //        case ECompMode.LessOrEqual: return v1 <= v2;
-        //        case ECompMode.Equal: return v1 == v2;
-        //        case ECompMode.GreaterOrEqual: return v1 >= v2;
-        //        case ECompMode.Greater: return v1 > v2;
-        //        case ECompMode.NotEqual: return v1 != v2;
-        //        default:
-        //            Debug.LogWarning("Compare failed: " + ParamName);
-        //            return false;
-        //    }
-        //}
     }
 
     public class ParamScalar : StateParam
     {
-        public ParamScalar(string name, float v) : base(name, EParamType.Scalar)
+        public ParamScalar(string name, float v) : base(name, EDBParamType.Scalar)
         {
             ParamValue = v;
         }
@@ -172,7 +168,7 @@ public class DBStateMachine
 
     public class ParamInt : StateParam
     {
-        public ParamInt(string name, int v) : base(name, EParamType.Integer)
+        public ParamInt(string name, int v) : base(name, EDBParamType.Integer)
         {
             ParamValue = v;
         }
@@ -181,12 +177,17 @@ public class DBStateMachine
 
     public class ParamBool : StateParam
     {
-        public ParamBool(string name, bool v) : base(name, EParamType.Bool)
+        public ParamBool(string name, bool v) : base(name, EDBParamType.Bool)
         {
             ParamValue = v;
         }
         public bool ParamValue;
     }
+
+
+    private Dictionary<string, StateParam> _parameters;
+    private Dictionary<string, DBState> _states;
+    private DBState _currentState;
 
     public bool DoseParamExist(string paramName)
     {
@@ -237,6 +238,25 @@ public class DBStateMachine
     public bool RemoveParam(string name)
     {
         return _parameters.Remove(name);
+    }
+
+    public void AddNewState(string name)
+    {
+        if (!_states.ContainsKey(name))
+            _states.Add(name, new DBState());
+    }
+
+    public void RemoveState(string name)
+    {
+        if (_states.ContainsKey(name))
+            _states.Remove(name);
+    }
+
+    private DBState GetState(string name)
+    {
+        DBState currState;
+        _states.TryGetValue(name, out currState);
+        return currState;
     }
 
     private void LogKeyConfliction(string paramName)
